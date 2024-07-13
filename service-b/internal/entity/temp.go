338bd80e424cf4787e29c072dac3479ba2aa31c8
GiovaniGitHub <giovani.angelo@gmail.com>
2024-07-13 18:11:00 -0300

    Update

---
+++ service-b/internal/entity/temp.go
@@ -0,0 +1,15 @@
+package entity
+
+type Temperature struct {
+	City  string `json:"city"`
+	TempC string `json:"temp_C"`
+	TempF string `json:"temp_F"`
+	TempK string `json:"temp_K"`
+}
+
+type WeatherData struct {
+	CurrentCondition []struct {
+		TempC string `json:"temp_C"`
+		TempF string `json:"temp_F"`
+	} `json:"current_condition"`
+}
